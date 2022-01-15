35b8e8bd99f6a2783c60b51273d63b36a257ae67
Mary Benjamin <marybenjamin713@gmail.com>
2022-01-14 18:09:34 -0600

    edit to substitution

--- src/substitution.js
+++ src/substitution.js
@@ -37,26 +37,33 @@ function testDuplicate(alphArr) { //a function to test any duplicates
 // console.log(testDuplicate(alphabetArr))
 if (testDuplicate(alphabetArr) === true) return false; //if duplicate is present false is returned
 
-
-let result = ''; //final message
-let combinedArrEncode = [...letterArr,...alphabetArr] //both letters and alphabet arrays are combined to one array used in encoding
-const combinedArrDecode = [...alphabetArr,...letterArr]; //same as above but in reverse order used in decoding
+/*
+// let combinedArrEncode = [...letterArr,...alphabetArr] //both letters and alphabet arrays are combined to one array used in encoding
+// const combinedArrDecode = [...alphabetArr,...letterArr]; //same as above but in reverse order used in decoding
 // console.log(combinedArr);
+*/
+let result = ''; //final message
 inputArr.forEach((input)=>{ //looping through each input
 
     if(encode){ //if encoding is true
         let initialIndex = letterArr.findIndex((letter)=>input === letter) //finds index if input matches the letters array
         if(initialIndex === -1) return result+=input //in case index cannot be found the input is added to the result as it is
+        return result += alphabetArr[initialIndex]; //alphabet with initial index is found
+      }
+        /*
         //as combined array has both letters and alphabet array, adding index found + 26 will yield the desired result
-        return result += combinedArrEncode[initialIndex + 26]; 
-        }
+        // return result += combinedArrEncode[initialIndex + 26]; 
+        */
 
     if(!encode) { //when decoding
         let initialIndex = alphabetArr.findIndex((alpha)=>input === alpha) //finds index if input matches the alphabet array
         console.log(initialIndex);
         if(initialIndex === -1) return result+=input //in case index cannot be found the input is added to the result as it is
+        return result += letterArr[initialIndex]; //letter with initial index is found
+        /*
         //as combined array has both alphabet and letters array(order flipped from encoding), adding index found + 26 will yield the desired result
-        return result += combinedArrDecode[initialIndex + 26];
+        // return result += combinedArrDecode[initialIndex + 26];
+        */
     }
             // console.log(initialIndex);
             // console.log(initialIndex);
